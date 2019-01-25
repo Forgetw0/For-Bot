412d0f44b4286336292aa73a07ef36791178a679
Forgetw0 <47029406+Forgetw0@users.noreply.github.com>
2019-01-25 15:00:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-const Discord = require('discord.js')
-const client = new Discord.Client();
+const Discord = require('discord.js');
+const fs = require('fs')
+const bot = new Discord.Client();
+const config = require('./config.json');
+bot.commands = new Discord.Collection()
 
-client.on('ready', () => {
-     console.log('Я готов!');
-     });
-     
-     client.on('message', message => {
-         if (message.content === 'help') {
-           messega.reply('/mute [участник] [причина] /ban [участник] [причина] /kick [участник] [причина] /send [текст] /serverinfo /prune [число](очищает чат)');
-                });
-                
-                
-                
-                clitent.login(process.env.BOT_TOKEN);
+fs.readdir('./commands', (err, files) => {
+    if (err) console.log(err)
+    let jsfile = files.filter(f => f.split('.').pop() === 'js')
+    if (jsfile.length <= 0) return console.log('Команды не найдены!')
+    console.log(`Loaded ${jsfile.length} commands`)
+    jsfile.forEach((f) => {
+        let props = require(`./commands/${f}`)
+        bot.commands.set(props.help.name, props)
+  })
+})
+
+bot.on('message', async message => {
+    let prefix = config.prefix;
+    let messageArray = message.content.split(' ')
+    let command = messageArray[0]
+    let args = messageArray.slice(1)
