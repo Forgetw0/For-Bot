5be772fa9542edfd24f71fe4879388656ce1f920
Forgetw0 <47029406+Forgetw0@users.noreply.github.com>
2019-02-06 22:06:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,24 @@
-const Discord = require('discord.js');
-const fs = require('fs')
-const bot = new Discord.Client();
-const config = require('./config.json');
-bot.commands = new Discord.Collection()
+const Discord = require('discord.js')
+const fs = require('fs') // подключаем fs к файлу
+const bot = new Discord.Client()
+const config = require('./config.json')
+bot.commands = new Discord.Collection() // создаём коллекцию для команд
+const exp = require('./functions/exp.js')
 
-fs.readdir('./commands', (err, files) => {
+fs.readdir('./commands', (err, files) => { // чтение файлов в папке commands
     if (err) console.log(err)
-    let jsfile = files.filter(f => f.split('.').pop() === 'js')
-    if (jsfile.length <= 0) return console.log('Команды не найдены!')
+
+    let jsfile = files.filter(f => f.split('.').pop() === 'js') // файлы не имеющие расширение .js игнорируются
+    if (jsfile.length <= 0) return console.log('Команды не найдены!') // если нет ни одного файла с расширением .js
+
     console.log(`Loaded ${jsfile.length} commands`)
-    jsfile.forEach((f) => {
+    jsfile.forEach((f, i) => { // добавляем каждый файл в коллекцию команд
         let props = require(`./commands/${f}`)
         bot.commands.set(props.help.name, props)
-  })
+    })
 })
 
-bot.on('message', async message => {
-    let prefix = config.prefix;
-    let messageArray = message.content.split(' ')
-    let command = messageArray[0]
-    let args = messageArray.slice(1)
 
-    let command_file = bot.commands.get(command.slice(prefix.length))
-    if (command_file) command_file.run(bot, message, args)
-    
-    if (message.content.startsWith(prefix + 'help')) {
-        message.channel.send('/mute [участник] [причина] /ban [участник] [причина] /kick [участник] [причина] /send [текст] /serverinfo /prune [число](очищает чат)')
-    }
-});
-
-bot.login(config.token);
-bot.on('ready', () => {
-    console.log(`${bot.user.username} online`);
-bot.user.setPresence({status: 'dnd', game:{name: 'Префикс "/" By Fazion', type: 0}});
-});
+bot.login(config.token)
+client.login(procces.env.BOT_TOKEN)
+})
